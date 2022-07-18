caca955a18ff0243de6babe997d5ff4173fed6d9
ark1987s <arkadiy1987@yandex.ru>
2022-07-18 06:11:00 +0300

    update

--- namespace ITMO.CSCourse2022.WinApp.Exam/Form1.Designer.cs
+++ namespace ITMO.CSCourse2022.WinApp.Exam/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,11 +43,33 @@
             this.dataGridView.Size = new System.Drawing.Size(800, 400);
             this.dataGridView.TabIndex = 0;
             // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(189, 415);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(112, 23);
+            this.btnDel.TabIndex = 1;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.butUpd_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDel);
             this.Controls.Add(this.dataGridView);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +82,8 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- namespace ITMO.CSCourse2022.WinApp.Exam/Form1.cs
+++ namespace ITMO.CSCourse2022.WinApp.Exam/Form1.cs
@@ -10,46 +10,31 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace namespace_ITMO.CSCourse2022.WinApp.Exam
-{
-    public partial class Form1 : Form
+
+{    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();            
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             using (SqlConnection conn = new SqlConnection("Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = test_block;" +
                 "Data Source = A-NB\\SQLEXPRESS"))
             {
-                // Define a t-SQL query string that has a parameter for orderID.
                 const string sql = "SELECT * FROM [dbo].[t1]";
 
-                // Create a SqlCommand object.
                 using (SqlCommand sqlCommand = new SqlCommand(sql, conn))
                 {
-                    // Define the @orderID parameter and set its value.
-                    //sqlCommand.Parameters.Add(new SqlParameter("@orderID", SqlDbType.Int));
-                    //sqlCommand.Parameters["@orderID"].Value = parsedOrderID;
-
                     try
                     {
                         conn.Open();
 
-                        // Run the query by calling ExecuteReader().
                         using (SqlDataReader read = sqlCommand.ExecuteReader())
                         {
-                            // Create a data table to hold the retrieved data.
                             DataTable dataTable = new DataTable();
-
-                            // Load the data from SqlDataReader into the data table.
                             dataTable.Load(read);
-
-                            // Display the data from the data table in the data grid view.
                             this.dataGridView.DataSource = dataTable;
-
-                            // Close the SqlDataReader.
                             read.Close();
                         }
                     }
@@ -58,12 +43,44 @@ namespace namespace_ITMO.CSCourse2022.WinApp.Exam
                         MessageBox.Show("The requested order could not be loaded into the form.");
                     }
                     finally
-                    {
-                        // Close the connection.
+                    {                        
                         conn.Close();
                     }
                 }
             }
+        } 
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            string conn_string = @"Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = test_block;" +
+                "Data Source = A-NB\\SQLEXPRESS";            
+            int index;
+            string num_table;
+            index = dataGridView.CurrentRow.Index;
+            num_table = Convert.ToString(dataGridView[0, index].Value);
+            string cmd_text = "DELETE FROM [dbo].[t1] WHERE [ID] = '" + num_table + "'";
+
+            SqlConnection sql_conn = new SqlConnection(conn_string);
+            SqlCommand sql_comm = new SqlCommand(cmd_text, sql_conn);
+
+            try
+            {
+                sql_conn.Open();
+                sql_comm.ExecuteNonQuery();
+            }
+            catch
+            {
+                MessageBox.Show("The requested order could not be deleted into the form.");
+            }
+            finally
+            {
+                sql_conn.Close();
+            }            
+        }
+
+        private void butUpd_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);           
         }
     }    
 }
